44beed34a8f5b402f92d29caad3d4e05cf6f5d7e
Darron Moraes <darron.dev@gmail.com>
2025-02-01 21:29:13 +0530

    initial commit

---
+++ src/main/java/in/nineteen96/aws_candolim/AwsCandolimApplication.java
@@ -0,0 +1,13 @@
+package in.nineteen96.aws_candolim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCandolimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCandolimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nineteen96/aws_candolim/AwsCandolimApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nineteen96.aws_candolim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCandolimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
